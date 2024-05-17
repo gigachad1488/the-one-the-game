765d59b63c6de6a34811ad0045a5a37de962d964
repack by iluhich <iluhadolmatov@bk.ru>
2024-05-17 17:26:17 +0300

    eye boss v0.1

--- Assets/MyAssets/Enemy/EyeBoss/EyeBoss.cs
+++ Assets/MyAssets/Enemy/EyeBoss/EyeBoss.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Health))]
-public class CubeBoss : MonoBehaviour, IBoss, IBossDamage
+public class EyeBoss : MonoBehaviour, IBoss, IBossDamage
 {
     public StateMachine stateMachine;
 
@@ -14,14 +14,10 @@ public class CubeBoss : MonoBehaviour, IBoss, IBossDamage
 
     public Rigidbody2D rb;
 
-    public Player aggroedPlayer {get; set;}
+    [SerializeField]
+    public Player aggroedPlayer { get; set; }
 
     public float mult = 1;
-
-    public CubeBordersFallAttack[] borderColliders;
-    public Thruster[] thrusters;
-
-    public AudioSource musicSource;
 
     public float firstPhaseMult = 1.2f;
     public float secondPhaseMult = 1.8f;
@@ -29,15 +25,12 @@ public class CubeBoss : MonoBehaviour, IBoss, IBossDamage
 
     public LayerMask groundLayer;
     public int damage = 50;
+
     [SerializeField]
     public int baseDamage { get; set; } = 50;
 
     [SerializeField]
     public float difficultyMult { get; set; } = 1.5f;
-
-    [Space(5)]
-    [Header("Attacks")]  
-    public ShockWaveAttack shockWaveAttackPrefab;
 
     [Space(5)]
     [Header("Death")]
@@ -49,6 +42,8 @@ public class CubeBoss : MonoBehaviour, IBoss, IBossDamage
 
         health.OnDamage += Phases;
         health.OnDeath += Health_OnDeath;
+
+        //aggroedPlayer = GameObject.FindGameObjectWithTag("aboba").GetComponent<Player>();     
     }
 
     private void Health_OnDeath()
@@ -59,25 +54,19 @@ public class CubeBoss : MonoBehaviour, IBoss, IBossDamage
 
     private IEnumerator Start()
     {
-        foreach (var item in borderColliders)
-        {
-            item.canCollisionAttack = false;
-        }
-
         health.maxHealth = System.Convert.ToInt32(health.maxHealth * difficultyMult);
         health.currentHealth = health.maxHealth;
 
         GetComponent<HealthBar>().UpdateHealthBar();
 
-        yield return new WaitForSeconds(2);    
-        attackStates.Add(new CubeRollState(stateMachine, this));
-        attackStates.Add(new CubeJumpState(stateMachine, this));
-        attackStates.Add(new CubeDashState(stateMachine, this));
+        yield return new WaitForSeconds(0.5f);
+
+        attackStates.Add(new EyeDashState(stateMachine, this));
         stateMachine.SetState(attackStates[0]);
 
         firstPhaseMult *= difficultyMult;
         secondPhaseMult *= difficultyMult;
-        thirdPhaseMult *= difficultyMult;       
+        thirdPhaseMult *= difficultyMult;
 
         mult = firstPhaseMult;
         baseDamage = System.Convert.ToInt32(baseDamage * difficultyMult);
@@ -109,7 +98,7 @@ public class CubeBoss : MonoBehaviour, IBoss, IBossDamage
         {
             this.mult = secondPhaseMult;
         }
-        else if (this.mult >= secondPhaseMult && this.mult < thirdPhaseMult &&  health.currentHealth <= health.maxHealth * 0.3f)
+        else if (this.mult >= secondPhaseMult && this.mult < thirdPhaseMult && health.currentHealth <= health.maxHealth * 0.3f)
         {
             this.mult = thirdPhaseMult;
         }
---
+++ Assets/MyAssets/Enemy/EyeBoss/EyeDashState.cs
@@ -0,0 +1,115 @@
+﻿using PrimeTween;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeDashState : BaseState
+{
+    public EyeBoss eyeBoss;
+
+    private float baseDashDistance = 45f;
+    private float dashDistance;
+
+    private float baseDashTime = 1.2f;
+    private float dashTime;
+    private float dashTimeTimer;
+
+    private float baseDashCd = 2f;
+    private float dashCd;
+    private float dashCdTimer;
+
+    private bool dashStarted = false;
+    private bool beginRotate = true;
+
+    private Vector2 dashDestination;
+    private Vector2 startPosition;
+
+    private float rotateTime;
+
+    private float currentAngle;
+
+    public EyeDashState(StateMachine stateMachine, EyeBoss eyeBoss) : base(stateMachine)
+    {
+        this.eyeBoss = eyeBoss;
+    }
+
+    public override void OnEnter()
+    {
+        dashDistance = baseDashDistance * eyeBoss.mult * 0.5f;
+        dashTime = baseDashTime / eyeBoss.mult * 0.5f;
+        dashCd = baseDashCd / eyeBoss.mult * 0.5f;
+
+        rotateTime = 0;
+
+        dashTimeTimer = 0f;
+        dashCdTimer = dashCd;
+        dashStarted = false;
+        beginRotate = true;
+
+        currentAngle = eyeBoss.rb.rotation;
+    }
+
+    public override void OnExit()
+    {
+
+    }
+
+    public override void OnUpdate()
+    {
+        if (dashCdTimer <= 0f) 
+        {
+            if (!dashStarted) 
+            {
+                dashStarted = true;
+
+                Vector3 direction = (eyeBoss.aggroedPlayer.gameObject.transform.position - eyeBoss.transform.position).normalized;
+                Vector3 fixedDirection = direction * dashDistance;
+
+                dashDestination = (eyeBoss.transform.position + (Vector3)eyeBoss.aggroedPlayer.playerMovement.rb.velocity.normalized * 4f) + fixedDirection;
+                startPosition = eyeBoss.rb.position;              
+
+                dashTimeTimer = 0;
+            }
+            
+            float ratio = dashTimeTimer / dashTime;
+
+            Vector3 nextPosition = Vector3.Lerp(startPosition, dashDestination, ratio);
+            eyeBoss.rb.MovePosition(nextPosition);
+
+            dashTimeTimer += Time.deltaTime;
+
+            if (ratio >= 0.99f)
+            {
+                dashStarted = false;
+                dashCdTimer = dashCd;
+                currentAngle = eyeBoss.rb.rotation;
+                rotateTime = 0;
+            }
+        }
+        else
+        {
+            dashCdTimer -= Time.deltaTime;
+            rotateTime += Time.deltaTime;
+
+            float ratio = rotateTime / dashCd;
+
+            float angle = Mathf.Atan2(eyeBoss.aggroedPlayer.transform.position.y - eyeBoss.transform.position.y, eyeBoss.aggroedPlayer.transform.position.x - eyeBoss.transform.position.x) * Mathf.Rad2Deg;
+            float nextAngle = Mathf.Lerp(currentAngle, angle, ratio);
+
+            eyeBoss.rb.MoveRotation(nextAngle);
+        }
+    }
+
+    private void RotateToPlayer(float time)
+    {
+        float angle = Mathf.Atan2(eyeBoss.aggroedPlayer.transform.position.y - eyeBoss.transform.position.y, eyeBoss.aggroedPlayer.transform.position.x - eyeBoss.transform.position.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, time);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(dashDestination, 50);
+    }
+}
